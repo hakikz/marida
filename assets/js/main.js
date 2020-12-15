26f68dac348a00a217a94c76c116f7d0a8136a37
hakikz <hakikzaman@gmail.com>
2020-12-16 00:26:34 +0600

    smooth scroll for href link initialized

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,6 +15,45 @@
             $('html, body').animate({ scrollTop: 0 }, 800);
             return false;
         });
+
+        // SMOOTH SCROLLING TO ANCHORS
+
+        $('a[href*="#"]')
+            // Remove links that don't actually link to anything
+            .not('[href="#"]')
+            .click(function (event) {
+                // On-page links
+                if (
+                    location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
+                    location.hostname == this.hostname
+                ) {
+                    // Figure out element to scroll to
+                    var target = $(this.hash);
+                    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                    // Does a scroll target exist?
+                    if (target.length) {
+                        // Only prevent default if animation is actually gonna happen
+                        event.preventDefault();
+                        $('html, body').animate({
+                            scrollTop: target.offset().top
+                        }, 1000, function () {
+                            //<!-- Start Comment Callback after animation
+                            // Must change focus! /End Comment-->
+                            var $target = $(target);
+                            $target.focus();
+                            if ($target.is(":focus")) { // Checking if the target was focused
+                                return false;
+                            } else {
+                                $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+                                $target.focus(); // Set focus again
+                            };
+
+                        });
+
+                    }
+                }
+            });
+
     });
 
 }(jQuery));
