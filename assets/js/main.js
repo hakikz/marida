22c59bdb7bdbd7d1e40b9cef2aeb2f20f5151215
hakikz <hakikzaman@gmail.com>
2020-12-16 12:45:20 +0600

    Navbar issue fixed for every screen

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,7 @@
 (function ($) {
 
     $(document).ready(function () {
-        $('#carouselHero').carousel({
-            pause: "false"
-        });
+        
         //Check to see if the window is top if not then display button
         $(window).scroll(function () {
             if ($(this).scrollTop() > 100) {
